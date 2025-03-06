fa3c817371a571592f2b7a666efd012317409927
Seyha Kruy <seyhakruy168168@gmail.com>
2025-03-06 15:34:18 +0700

    Done

---
+++ Start code/exercise-5/src/data.js
@@ -0,0 +1,43 @@
+/* Your data here */
+import sok from "./assets/sok.png";
+import sovan from "./assets/sovan.png";
+import jame from "./assets/jame.png";
+import nin from "./assets/nin.png";
+export const Information = [
+    {
+        name : "Dang",
+        title : "My daily life",
+        description: "Try something you've always been curious about, whether it's painting, cooking, coding, gardening, or playing a musican instrument. Hobbies can be a great way to relax and discover new passions",
+        image: {
+            src :sok,
+            alt : "pic1"
+        }
+    },
+    {
+        name : "Ding",
+        title : "IT Trainer",
+        description: "Try something you've always been curious about, whether it's painting, cooking, coding, gardening, or playing a musical instrument. Hobbies can be a great way to relax and discover new passions",
+        image: {
+            src :sovan,
+            alt : "pic2"
+        }
+    },
+    {
+        name : "Dung",
+        title : "IT Trainer",
+        description: "Try something you've always been curious about, whether it's painting, cooking, coding, gardening, or playing a musical instrument. Hobbies can be a great way to relax and discover new passions",
+        image: {
+            src :jame,
+            alt : "pic3"
+        }
+    },
+    {
+        name : "Dong",
+        title : "IT Trainer",
+        description: "Try something you've always been curious about, whether it's painting, cooking, coding, gardening, or playing a musical instrument. Hobbies can be a great way to relax and discover new passions",
+        image: {
+            src :nin,
+            alt : "pic4"
+        }
+    },
+  ];
